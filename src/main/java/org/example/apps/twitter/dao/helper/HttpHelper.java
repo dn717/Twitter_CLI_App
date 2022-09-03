7a3654b299d7ed28437058afa89881205f2bc4e5
dn717 <djiang3@lakeheadu.ca>
2022-09-02 22:13:18 -0400

    create twitter command line tool app

---
+++ src/main/java/org/example/apps/twitter/dao/helper/HttpHelper.java
@@ -0,0 +1,22 @@
+package org.example.apps.twitter.dao.helper;
+
+import java.net.URI;
+import org.apache.http.HttpResponse;
+import org.apache.http.entity.StringEntity;
+
+public interface HttpHelper {
+    /**
+     * Execute a HTTP Post call
+     * @param uri
+     * @return
+     */
+    HttpResponse httpPost(URI uri);
+
+    /**
+     * Execute a HTTP Get call
+     * @param uri
+     * @return
+     */
+    HttpResponse httpGet(URI uri);
+
+}
